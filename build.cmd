25fd9dc598e7471e866f6819712b708c9e9d9359
msamoylenko <ghost1212@mail.ru>
2016-11-01 18:04:32 +0500

    Fixed build script to run on team city.

--- build.cmd
+++ build.cmd
@@ -1,15 +1,14 @@
 @echo off
-set dotNetBasePath=%windir%\Microsoft.NET\Framework
-if exist %dotNetBasePath%64 set dotNetBasePath=%dotNetBasePath%64
-for /R %dotNetBasePath% %%i in (*msbuild.exe) do set msbuild=%%i
 
+set buildToolsPath=%ProgramFiles(x86)%\MSBuild
+set msbuild="%buildToolsPath%\14.0\bin\amd64\MSBuild.exe"
 set sln=GroboTrace\GroboTrace.sln
-SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
+SET VCTargetsPath=%buildToolsPath%\Microsoft.Cpp\v4.0\V140
 
 set target=Output
 if exist %target% rd /s /q %target%
 mkdir %target%
 
-Assemblies\nuget.exe restore %sln%
+Assemblies\nuget.exe restore %sln%                                                    
 
 %msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %sln%
