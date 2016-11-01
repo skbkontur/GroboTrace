4d7928ab3406a40168d744f605f78cb31491b31c
msamoylenko <ghost1212@mail.ru>
2016-11-01 15:37:28 +0500

    fix build script to copy results to Output dir

--- build.cmd
+++ build.cmd
@@ -3,7 +3,13 @@ set dotNetBasePath=%windir%\Microsoft.NET\Framework
 if exist %dotNetBasePath%64 set dotNetBasePath=%dotNetBasePath%64
 for /R %dotNetBasePath% %%i in (*msbuild.exe) do set msbuild=%%i
 
-set target=GroboTrace\GroboTrace.sln
+set sln=GroboTrace\GroboTrace.sln
 SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
 
-%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %target%
+set target=Output
+if exist %target% rd /s /q %target%
+mkdir %target%
+
+Assemblies\nuget.exe restore %sln%
+
+%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %sln%
