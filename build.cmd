73d6647ae8de7566bcbe46cdf4c8c552edf3172d
andrew <Andrew.Kostousov@gmail.com>
2017-02-14 11:32:45 +0500

    fix build script

--- build.cmd
+++ build.cmd
@@ -3,12 +3,12 @@
 set buildToolsPath=%ProgramFiles(x86)%\MSBuild
 set msbuild="%buildToolsPath%\14.0\bin\amd64\MSBuild.exe"
 set sln=GroboTrace\GroboTrace.sln
-SET VCTargetsPath=%buildToolsPath%\Microsoft.Cpp\v4.0\V140
+set VCTargetsPath=%buildToolsPath%\Microsoft.Cpp\v4.0\V140
 
 set target=Output
-if exist %target% rd /s /q %target%
-mkdir %target%
+if exist %target% rd /s /q %target% || exit /b 1
+mkdir %target% || exit /b 1
 
-Assemblies\nuget.exe restore %sln%
+Tools\nuget.exe restore %sln% || exit /b 1
 
-%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %sln%
+%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %sln% || exit /b 1
